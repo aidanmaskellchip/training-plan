da737334677d302621430c8becdfd6d668328214
Aidan Maskell <aidan.maskell@getchip.uk>
2024-08-06 15:06:06 +0100

    refactored to use same channel between funcs and handled if no activity stats for user

--- internal/application/query/get_user_stats_query.go
+++ internal/application/query/get_user_stats_query.go
@@ -1,7 +1,9 @@
 package query
 
 import (
+	"errors"
 	"log"
+	"sync"
 	"training-plan/internal/domain/model"
 	vo "training-plan/internal/domain/value_objects"
 	"training-plan/internal/infrastructure/repository"
@@ -11,46 +13,62 @@ func GetUserStatsQuery(id *string, repos *repository.Repositories) (res []model.
 	userID := vo.NewUserID(*id)
 
 	statsChan := make(chan model.ActivityStats)
+	var wg sync.WaitGroup
+	wg.Add(4)
 
 	go func() {
 		act, err := repos.UserActivityRepository.GetFastestUserActivity(userID.ID)
 		if err != nil {
 			log.Println(err)
+			wg.Done()
 			return
 		}
 
 		statsChan <- act
+		wg.Done()
 	}()
 
 	go func() {
 		act, err := repos.UserActivityRepository.GetLongestUserActivity(userID.ID)
 		if err != nil {
 			log.Println(err)
+			wg.Done()
 			return
 		}
 
 		statsChan <- act
+		wg.Done()
 	}()
 
 	go func() {
 		act, err := repos.UserActivityRepository.GetFastestCommunityActivity()
 		if err != nil {
 			log.Println(err)
+			wg.Done()
 			return
 		}
 
 		statsChan <- act
+		wg.Done()
 	}()
 
 	go func() {
 		act, err := repos.UserActivityRepository.GetLongestCommunityActivity()
 		if err != nil {
 			log.Println(err)
+			wg.Done()
 			return
 		}
 
 		statsChan <- act
+		wg.Done()
 	}()
+
+	wg.Wait()
+
+	if len(statsChan) != 4 {
+		return res, errors.New("could not retrieve stats")
+	}
 
 	for i := 0; i < 4; i++ {
 		res = append(res, <-statsChan)
